c8790dff1cd6dd04768ef73f8beda2ce8370f46c
yogadarkknight <yoga.darkknight@gmail.com>
2023-08-21 17:03:46 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:aplikasi_tempat_wisata/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:aplikasi_tempat_wisata/detail_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: const MainScreen(),
+    );
+  }
+}
+
+
+
+
+
